23ce04b09e2e39bd20fa5cdac08cc37792309b5e
zwill22 <73105122+zwill22@users.noreply.github.com>
2026-06-29 11:44:18 +0100

    Moved blog post components to markdown format and added list components

--- components/markdown/format.tsx
+++ components/markdown/format.tsx
@@ -1,3 +1,8 @@
+import {
+  ArrowRightCircleIcon,
+  ChevronDoubleRightIcon,
+  ChevronRightIcon,
+} from "@heroicons/react/24/outline";
 import { Skeleton, Typography } from "@heroui/react";
 import clsx from "clsx";
 import { ReactNode, Suspense } from "react";
@@ -12,7 +17,7 @@ export function Heading(props: {
       type="h1"
       align={props.align ?? "start"}
       weight={props.weight ?? "normal"}
-      className="flex grow w-full text-5xl md:text-6xl font-heading p-2 text-pretty"
+      className="flex grow w-full text-5xl md:text-6xl font-heading py-4 md:py-8 text-pretty"
     >
       {props.children}
     </Typography>
@@ -30,6 +35,23 @@ export function Subheading(props: {
       align={props.align ?? "start"}
       weight={props.weight ?? "normal"}
       className="grow w-full text-2xl md:text-3xl font-heading py-3 md:py-6 text-pretty"
+    >
+      {props.children}
+    </Typography>
+  );
+}
+
+export function MinorHeading(props: {
+  children?: ReactNode;
+  align?: any;
+  weight?: any;
+}) {
+  return (
+    <Typography
+      type="h3"
+      align={props.align ?? "start"}
+      weight={props.weight ?? "normal"}
+      className="grow w-full text-xl md:text-2xl font-heading py-2 md:py-4 text-pretty"
     >
       {props.children}
     </Typography>
@@ -104,5 +126,26 @@ export function BlogImage(props: ImageProps) {
         />
       </div>
     </Suspense>
+  );
+}
+
+export function List(props: { children: ReactNode }) {
+  return <div className="flex flex-col gap-4 p-4">{props.children}</div>;
+}
+
+export function ListItem(props: { children: ReactNode; depth: number }) {
+  return (
+    <div className="flex gap-2 align-top">
+      <div className="flex flex-col size-5 justify-center h-8">
+        {props.depth === 1 ? (
+          <ArrowRightCircleIcon />
+        ) : props.depth === 2 ? (
+          <ChevronRightIcon />
+        ) : (
+          <ChevronDoubleRightIcon />
+        )}
+      </div>
+      <p className="md:text-lg justify-start leading-8">{props.children}</p>
+    </div>
   );
 }
