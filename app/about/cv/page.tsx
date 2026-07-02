9ebb3bdca32657706a8f00337d79a5dbe474d3a8
zwill22 <73105122+zwill22@users.noreply.github.com>
2026-07-02 20:44:14 +0100

    Added header to cv

--- app/about/cv/page.tsx
+++ app/about/cv/page.tsx
@@ -1,11 +1,19 @@
+import { Heading } from "@/components/typesetting/format";
 import { PageBreadcrumbs } from "@/components/ui/breadcrumbs";
 import { Section } from "@/components/ui/section";
+import { siteConfig } from "@/config/site";
 import { fetchCV } from "@/lib/cv";
 import { Link } from "@heroui/react";
 import clsx from "clsx";
+import { ReactNode } from "react";
 
-function PdfLink() {
-  const href = `https://www.icloud.com/iclouddrive/0bc2fZznjWFeMF2oswPpiupoA#Zack_Williams_PhD_-_Curriculum_Vitae`;
+function CVLink(props: {
+  href: string;
+  external: boolean;
+  children: ReactNode;
+}) {
+  const rel = props.external ? "noopener noreferrer" : undefined;
+  const target = props.external ? "_blank" : undefined;
 
   return (
     <div className="flex flex-col justify-center">
@@ -15,16 +23,91 @@ function PdfLink() {
           "shadow shadow-foreground/50 hover:ring hover:ring-foreground/50 hover:bg-foreground/20",
           "hover:ring-offset-1 hover:ring-offset-background",
         )}
-        href={href}
-        rel="noopener noreferrer"
-        target="_blank"
+        href={props.href}
+        rel={rel}
+        target={target}
       >
-        <p className={clsx("font-bold text-nowrap text-sm")}>PDF Version</p>
-        <div className="text-lg my-auto">
-          <i className="bi bi-filetype-pdf" />
-        </div>
+        {props.children}
       </Link>
     </div>
+  );
+}
+
+function PdfLink() {
+  const hrefPdf = `https://www.icloud.com/iclouddrive/0bc2fZznjWFeMF2oswPpiupoA#Zack_Williams_PhD_-_Curriculum_Vitae`;
+
+  return (
+    <CVLink href={hrefPdf} external={true}>
+      <p className={clsx("font-bold text-nowrap text-sm")}>PDF Version</p>
+      <div className="text-lg my-auto">
+        <i className="bi bi-filetype-pdf" />
+      </div>
+    </CVLink>
+  );
+}
+
+function Email() {
+  return (
+    <CVLink href="/about?showCompose=true#contactme" external={false}>
+      <div className="text-lg my-auto">
+        <i className="bi bi-envelope" />
+      </div>
+      <p className={clsx("font-bold font-plain text-nowrap text-sm")}>
+        Send Message
+      </p>
+    </CVLink>
+  );
+}
+
+function LinkedIn() {
+  return (
+    <CVLink href={siteConfig.links.linkedin} external={true}>
+      <div className="text-lg my-auto">
+        <i className="bi bi-linkedin" />
+      </div>
+      <p
+        className={clsx(
+          "font-bold font-plain text-nowrap text-sm text-right w-18",
+        )}
+      >
+        LinkedIn
+      </p>
+    </CVLink>
+  );
+}
+
+function GitHub() {
+  return (
+    <CVLink href={siteConfig.links.github} external={true}>
+      <div className="text-lg my-auto">
+        <i className="bi bi-github" />
+      </div>
+      <p
+        className={clsx(
+          "font-bold font-plain text-nowrap text-sm text-right w-16",
+        )}
+      >
+        GitHub
+      </p>
+    </CVLink>
+  );
+}
+
+function Phone() {
+  return <></>;
+}
+
+function CVHeader() {
+  return (
+    <>
+      <Heading>Zack M Williams PhD</Heading>
+      <div className="flex w-full gap-4">
+        <Email />
+        <LinkedIn />
+        <GitHub />
+        <Phone />
+      </div>
+    </>
   );
 }
 
@@ -42,6 +125,8 @@ export default async function CV() {
         <PageBreadcrumbs crumbs={crumbs} back="/about" current="CV" />
         <PdfLink />
       </div>
+
+      <CVHeader />
 
       <div className="w-full text-left">{cvComponent}</div>
     </Section>
