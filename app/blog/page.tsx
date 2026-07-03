b5ee3acff408073bfe085e5064f1890276b9d8a2
zwill22 <73105122+zwill22@users.noreply.github.com>
2026-07-03 11:21:53 +0100

    Added introduction paragraph to blog and projects and moved fetch functions behind suspense

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,18 +2,35 @@ import { Section, SectionTitle } from "@/components/ui/section";
 import { fetchBlogPosts } from "@/lib/blogs";
 import { ListMenu } from "@/components/menu/menu-list";
 import { PageBreadcrumbs } from "@/components/ui/breadcrumbs";
+import { Paragraph } from "@/components/typesetting/format";
+import { Suspense } from "react";
+import { ListSkeleton } from "@/components/menu/skeletons";
 
-export default async function BlogMenu() {
+async function BlogList() {
   const posts = await fetchBlogPosts();
+
+  return <ListMenu items={posts} href="/blog" />;
+}
+
+export default function BlogMenu() {
   const crumbs = [{ name: "Home", href: "/" }];
 
   return (
     <Section>
       <PageBreadcrumbs crumbs={crumbs} back="/" current="Blog" />
       <SectionTitle>Blog Posts</SectionTitle>
-      
+
+      <Paragraph className="w-full px-2 md:px-4">
+        One of the primary reasons for setting up this site was for their to be
+        somewhere where I could write articles about any topic. Though I do this
+        infrequently, having somewhere to put these would give me more reason to
+        write.
+      </Paragraph>
+
       <div className="w-full">
-        <ListMenu items={posts} href="/blog" />
+        <Suspense fallback={<ListSkeleton length={4} />}>
+          <BlogList />
+        </Suspense>
       </div>
     </Section>
   );
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,11 +1,18 @@
 import { ListMenu } from "@/components/menu/menu-list";
+import { ListSkeleton } from "@/components/menu/skeletons";
 import { PageBreadcrumbs } from "@/components/ui/breadcrumbs";
 import { Section, SectionTitle } from "@/components/ui/section";
 import { fetchProjects } from "@/lib/projects";
+import { Paragraph } from "@heroui/react/typography";
+import { Suspense } from "react";
 
-export default async function Projects() {
+async function ProjectList() {
   const projects = await fetchProjects();
 
+  return <ListMenu items={projects} href="/projects" />;
+}
+
+export default function Projects() {
   const crumbs = [{ name: "Home", href: "/" }];
 
   return (
@@ -13,8 +20,16 @@ export default async function Projects() {
       <PageBreadcrumbs crumbs={crumbs} back="/" current="Projects" />
       <SectionTitle>My Projects</SectionTitle>
 
+      <Paragraph className="w-full px-2 md:px-4">
+        The following are projects that I have worked on in my free time. All
+        are open-source and available on my GitHub. If you have any questions or
+        suggestions about these, I am always willing to help.
+      </Paragraph>
+
       <div className="w-full">
-        <ListMenu items={projects} href="/projects" />
+        <Suspense fallback={<ListSkeleton length={4} />}>
+          <ProjectList />
+        </Suspense>
       </div>
     </Section>
   );
